5a47c2da29cf88690db75e23a2d91af567d0b230
saulvaldelvira <saulvaldelvira@gmail.com>
2022-12-17 17:47:05 +0100

    Day 8

--- 2022/Day08/puzzle8.c
+++ 2022/Day08/puzzle8.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILENAME "input.txt"
+#define FILENAME "test.txt"
 #define MATRIX_SIZE 100
 
 int is_visible(int x, int y, int matrix[MATRIX_SIZE][MATRIX_SIZE], int row, int col){
