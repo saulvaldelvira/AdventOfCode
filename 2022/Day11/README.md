f33c7b5ef0a72f44416523bf33870f36c99ef3a8
saulvaldelvira <saulvaldelvira@gmail.com>
2022-12-18 17:52:38 +0100

    Day 11 | And i thought Day 7 was hard..

---
+++ 2022/Day11/README.md
@@ -0,0 +1,43 @@
+This puzzle is a little bit tricky and uses modular algebra. <br>
+Here bellow is an explanation from <a href="https://www.reddit.com/user/MattieShoes/">u/MattieShoes</a> on Reddit. <br>
+
+There's different levels of understanding, but... We can start from easy.
+
+If we're testing for divisibility by 2
+
+  0 [2]  1 []
+  2 [2]  3 []
+  4 [2]  5 []
+  6 [2]  7 []
+...
+
+It's pretty easy to spot the pattern -- it repeats after 2 entries. So you can alter the dividend (worry) by adding or subtracting 2 as much as you like, and it won't affect the result of the divisibility test.
+
+Then you have another monkey that tests for divisibility by three though, so adding or subtracting by 2 would affect the later disposition of that item if it ever goes to that monkey. So we need to find the number we could add or subtract by that won't affect divisibility by either
+
+  0 [2, 3]  1 []  2 [2]  3 [3]  4 [2]  5 []
+  6 [2, 3]  7 []  8 [2]  9 [3] 10 [2] 11 []
+ 12 [2, 3] 13 [] 14 [2] 15 [3] 16 [2] 17 []
+ 18 [2, 3] 19 [] 20 [2] 21 [3] 22 [2] 23 []
+...
+
+You can see there's a pattern and it repeats after 6 entries. So you can alter the dividend (worry) by adding or subtracting 6 freely and it won't affect the divisibility test for either monkey. Now 6 happens to be 2 x 3 so you might want to check that, say, using 3 and 5.
+
+  0 [3, 5]  1 []  2 []  3 [3]  4 []  5 [5]  6 [3]  7 []  8 []  9 [3] 10 [5] 11 [] 12 [3] 13 [] 14 []
+ 15 [3, 5] 16 [] 17 [] 18 [3] 19 [] 20 [5] 21 [3] 22 [] 23 [] 24 [3] 25 [5] 26 [] 27 [3] 28 [] 29 []
+ 30 [3, 5] 31 [] 32 [] 33 [3] 34 [] 35 [5] 36 [3] 37 [] 38 [] 39 [3] 40 [5] 41 [] 42 [3] 43 [] 44 []
+...
+
+Sho nuff, the cycle is 15.
+
+Now for certain pairs of numbers, the pattern will be shorter than the product of the numbers -- e.g. for 2 and 4, the pattern is 4 long, not 8 -- but the shorter pattern always fits evenly into the product, so using the product still works. So there's a lot of stuff you can uncover (https://en.wikipedia.org/wiki/Coprime_integers) but you don't need it to get the right answer.
+
+So at this point, you can solve the problem without using modulo at all -- get the cycle length for all 8 monkeys combined (the product of their divisors), then you could do something ghetto like
+
+while worry > cycle_length:
+    worry -= cycle_length
+
+and you know you haven't affected any of the divisibility tests.
+
+Modulo worry = worry % cycle_length is just turning the loop above into a single operation. It's prettier, but it's the same thing.
+
---
+++ 2022/Day11/puzzle11.c
@@ -0,0 +1,242 @@
+#include "file.h"
+#include <string.h>
+#include <stdlib.h>
+#define DEBUG 0
+#if DEBUG
+    #define FILENAME "test.txt"
+#else
+    #define FILENAME "input.txt"
+#endif
+
+
+int cycle_length;
+int divisors[MAX_BUFFER_SIZE];
+int divisiors_length;
+
+struct Monkey{
+    int id;
+    int items[MAX_BUFFER_SIZE];
+    int nItems;
+    char operator;
+    char operand[MAX_BUFFER_SIZE];
+    int divisible_test;
+    int true_case,
+        false_case;
+    int nInspections;
+};
+
+void innit_monkey(char line[], FILE *file, struct Monkey monkeys[], int *mLenght, int *status){
+
+    #if DEBUG
+        printf("Inniting monkey\n");
+    #endif
+
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);//the first line if just the monkey lenght
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);
+    monkeys[*mLenght].id = *mLenght;
+    char aux[MAX_BUFFER_SIZE];
+    int pos = 0;
+    #if DEBUG
+        printf("\tGetting items\n");
+    #endif
+
+    while(get_number(line, aux, pos++)!=-1){
+        monkeys[*mLenght].items[monkeys[*mLenght].nItems++] = atoi(aux);
+    }
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);
+
+    #if DEBUG
+        printf("\tGetting operand\n");
+    #endif
+
+    char op;
+    skip_multiple(line, aux, "*+/-" , 4, &op);
+    monkeys[*mLenght].operator = op;
+    aux[0] = ' ';
+    no_whitespace(aux, aux);
+    strcpy(monkeys[*mLenght].operand, aux);
+
+    #if DEBUG
+        printf("\tGetting divisibility\n");
+    #endif
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);
+    get_number(line, aux, 0);
+    monkeys[*mLenght].divisible_test = atoi(aux);
+
+    for(int i=0; i<divisiors_length; i++){
+        if(divisors[i]==monkeys[*mLenght].divisible_test)
+            continue;
+        cycle_length *= monkeys[*mLenght].divisible_test;
+        divisors[divisiors_length++] = monkeys[*mLenght].divisible_test;
+        break;
+    }
+
+    if(divisiors_length==0){
+        //printf("New divisior %d\n", monkeys[*mLenght].divisible_test);
+        cycle_length *= monkeys[*mLenght].divisible_test;
+        divisors[divisiors_length++] = monkeys[*mLenght].divisible_test;
+    }
+
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);
+    get_number(line, aux, 0);
+    monkeys[*mLenght].true_case = atoi(aux);
+
+
+    *status = fgetline(line, file, MAX_BUFFER_SIZE);
+    get_number(line, aux, 0);
+    monkeys[*mLenght].false_case = atoi(aux);
+
+
+    monkeys[*mLenght].nInspections = 0;
+
+    //printf("\tnew = old %c %s\n", monkeys[*mLenght].operator, monkeys[*mLenght].operand);
+
+    (*mLenght)++;
+
+    
+
+    #if DEBUG
+        printf("Finish\n");
+    #endif
+}
+
+void print_monkey(struct Monkey monkey){
+    printf("Monkey Nº%d\n\t", monkey.id);
+    for(int i=0; i<monkey.nItems; i++)
+        printf("%d ", monkey.items[i]);
+    printf("\n");
+}
+
+void turn(struct Monkey monkeys[], int nMonkey){
+    struct Monkey *current = &monkeys[nMonkey];
+    #if DEBUG
+    printf("\nMonkey %d:\n", current->id);
+    #endif
+    while(current->nItems>0){
+        #if DEBUG
+        printf("\tMonkey inspects an item with a worry level of %d.\n", current->items[0]);
+        #endif
+        current->nInspections++;
+
+        /**
+         * To prevent the values to overfow, we store them into 
+         *          a long. Also, we perform the modulus of the value and the least common
+         *          multiplier so the value stays in the [INT_MIN,INT_MAX] range without 
+         *          affecting the result of the modulus of that number and the divisors of the monkeys. 
+         * 
+         * See: https://www.reddit.com/r/adventofcode/comments/zizi43/2022_day_11_part_2_learning_that_it_was_that/
+         * 
+         */
+        long tmp = current->items[0];
+        tmp %= cycle_length;
+        int op;
+        if(strncmp(current->operand, "old", 3)==0)
+            op = current->items[0];
+        else
+            op = atoi(current->operand);
+
+
+        switch (current->operator)
+        {
+        case '+':
+            tmp = tmp + op;
+            break;
+        case '*':
+            tmp = tmp * op;
+            break;
+        case '-':
+            tmp = tmp - op;
+            break;
+        case '/':
+            tmp = tmp / op;
+            break;
+        default:
+            break;
+        }
+        #if DEBUG
+        printf("\tnew worry level: %d.\n", current->items[0]);
+        #endif
+        tmp %= cycle_length;
+        
+        current->items[0] = tmp;
+        if(current->items[0]%current->divisible_test == 0){
+            #if DEBUG
+            printf("\tSending %d to monkey %d\n", current->items[0], current->true_case);
+            #endif
+            monkeys[current->true_case].items[monkeys[current->true_case].nItems] = current->items[0];
+            monkeys[current->true_case].nItems++;
+        }else{
+            #if DEBUG
+            printf("\tSending %d to monkey %d\n", current->items[0], current->false_case);
+            #endif
+            monkeys[current->false_case].items[monkeys[current->false_case].nItems] = current->items[0];
+            monkeys[current->false_case].nItems++;
+        }
+
+        for(int i=0; i<current->nItems-1; i++)
+            current->items[i] = current->items[i+1];
+        current->nItems--;
+        #if DEBUG
+        printf("\n");
+        #endif
+    }
+}
+
+int main(){
+    FILE *file = fopen(FILENAME, "r");
+    if(!file){
+        fprintf(stderr, "Error: could not find filename %s\n", FILENAME);
+        return -1;
+    }
+    
+    char line[MAX_BUFFER_SIZE];
+
+    struct Monkey monkeys[MAX_BUFFER_SIZE];
+    int monkeys_lenght = 0;
+
+    int status = 1;
+    cycle_length = 1;
+    divisiors_length = 0;
+    
+    while (status != EOF){
+        innit_monkey(line, file, monkeys, &monkeys_lenght, &status);
+        if(status != EOF){
+            status = fgetline(line, file, MAX_BUFFER_SIZE);
+            #if DEBUG
+                    printf("This sould be empty (%s)\n", line);
+            #endif
+        }
+
+    }
+
+    printf("Cycle length: %d\n", cycle_length);
+    for(int i=0; i<divisiors_length; i++)
+        printf("%d ", divisors[i]);
+    printf("\n");
+    //return 0;
+
+    int nRounds = 10000;
+
+    for(int i=0; i<nRounds; i++){
+        #if DEBUG
+        printf("***********TURN %d*********\n", i+1);
+        #endif
+        for(int j=0; j<monkeys_lenght; j++)
+            turn(monkeys, j);
+    }
+        
+    int max1 = 0,
+        max2 = 0;
+    for(int i=0; i<monkeys_lenght; i++){
+        printf("%d\n", monkeys[i].nInspections);
+        if(monkeys[i].nInspections>max1){
+            max2 = max1;
+            max1 = monkeys[i].nInspections;
+        }else if(monkeys[i].nInspections>max2)
+            max2 = monkeys[i].nInspections;
+    }
+    printf("Most active %d and %d\n",max1, max2);
+    printf("Level of Monkey Business: %ld\n", ((long)max1)*max2);
+
+    return 1;
+}
